2614a3fc0395d31b3f018e023614eae2bbf31b02
Ahmed.KHOUAJA <ahmed.khouaja@berger-levrault.com>
2022-08-10 12:16:08 +0200

    Add firebase connection and error dialog

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:ak_food_vendeur/View/FirstScreen/first_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
